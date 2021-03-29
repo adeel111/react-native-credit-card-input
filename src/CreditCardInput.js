a777071eac8c84843b76e81bc92ff3aaf2872e7d
adeel11 <adeeliftikhar67@gmail.com>
2021-03-29 16:18:22 +0500

    changed orientation

--- src/CreditCardInput.js
+++ src/CreditCardInput.js
@@ -199,10 +199,10 @@ const POSTAL_CODE_INPUT_WIDTH = 120; // https://github.com/yannickcr/eslint-plug
         />
         <ScrollView
           ref="Form"
-          horizontal
+          // horizontal
           keyboardShouldPersistTaps="always"
           scrollEnabled={allowScroll}
-          showsHorizontalScrollIndicator={false}
+          showsVerticalScrollIndicator={false}
           style={s.form}
         >
           <CCInput
